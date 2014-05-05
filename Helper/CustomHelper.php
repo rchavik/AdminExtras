c83f514503a3524cd3f64ea8e7e5fefc90435502
Rachman Chavik <rchavik@xintesa.com>
2014-05-05 13:24:56 +0700

    Initialize datepicker for input-datetime and input-daterange

---
+++ Helper/CustomHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+App::uses('AppHelper', 'View/Helper');
+
+class CustomHelper extends AppHelper {
+
+	public $helpers = array(
+		'Html',
+	);
+
+	public function beforeRender($viewFile) {
+		$this->Html->css('datepicker', array('inline' => false));
+		$this->Html->script('bootstrap-datepicker', array('inline' => false));
+		$script = <<<EOF
+$('input.input-datetime, .input-daterange').datepicker({
+	autoclose: false,
+	showTime: true,
+	todayBtn: true,
+	todayHighlight: true,
+	clearBtn: true,
+	format: 'yyyy-mm-dd H:i:s'
+});
+EOF;
+		$this->Html->scriptBlock($script, array('block' => 'scriptBottom'));
+	}
+
+}
